8fef1ccd81a361c5c9ef1eadcab0342ac3f47c0d
Fidelis Adaikwu <110574901+DexFA@users.noreply.github.com>
2023-10-13 07:39:09 +0100

    Convert form into controlled element.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
 	{ id: 1, description: "Passports", quantity: 2, packed: false },
 	{ id: 2, description: "Socks", quantity: 12, packed: false },
+	{ id: 3, description: "Charger", quantity: 2, packed: true },
 ];
 
 export default function App() {
@@ -19,17 +22,38 @@ function Logo() {
 }
 
 function Form() {
+	const [description, setDescription] = useState("");
+	const [quantity, setQuantity] = useState(1);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const newItem = { description, quantity, packed: false, id: Date.now() };
+		console.log(newItem);
+
+		setDescription("");
+		setQuantity(1);
+	}
+
 	return (
-		<form className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your 😍 trip?</h3>
-			<select>
+			<select
+				value={quantity}
+				onChange={(e) => setQuantity(Number(e.target.value))}
+			>
 				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
 					<option value={num} key={num}>
 						{num}
 					</option>
 				))}
 			</select>
-			<input type="text" placeholder="Item..." />
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
 			<button>Add</button>
 		</form>
 	);
@@ -40,7 +64,7 @@ function PackingList() {
 		<div className="list">
 			<ul>
 				{initialItems.map((item) => (
-					<Item item={item} />
+					<Item item={item} key={item.id} />
 				))}
 			</ul>
 		</div>
